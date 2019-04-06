b1ecb390be9bdd77a48c590f9730e310e56034f8
riku hatakeyama <happy.riku8888@gmail.com>
2019-04-06 17:31:15 +0900

    hello, second

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+  <div>
+  <h1>{props.headerText}</h1>
+  <h1>{props.arbitraryPhrase}</h1>
+  </div>
+
+)
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{color: 'teal'}}>
+  <Header headerText="About Gatsby dayo" arbitraryPhrase="is arbitrary"/>
+   <Header headerText="It's pretty cool" />
+  <p>Such wow. Very React.</p>
+  </div>
+
+)
